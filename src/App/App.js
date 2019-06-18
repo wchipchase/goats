5aeaf1c890e5420b4006b13fc17adf2f5985a44b
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-06-17 19:22:39 -0600

    app is finished

---
+++ src/App/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import goats from './goats';
+
+import './App.scss';
+
+
+class App extends React.Component {
+// Data stuff goes here (axios calls to get data) Anything that modifies state.
+// Anything defined here requires this. syntax
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    this.setState({ goats });
+  }
+
+  render() {
+    // This is where you would iterate over an array. No need for this. syntax . {} is javascript
+    // no {} = string
+    return (
+      // This is where JSX is written (domstring stuff)
+      <div className='App'>
+        <div>Goat Yoga</div>
+        {/* <GoatCorral goats={goats}/> */}
+      </div>
+
+    );
+  }
+}
+
+export default App;
