c0714b1dc7d27e23c6345deaedb94a3e60cebf58
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-06-17 20:04:12 -0600

    fnished goatcorral

---
+++ src/components/GoatCorral/GoatCorral.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './GoatCorral.scss';
+
+class GoatCorral extends React.Component {
+  render() {
+    const { goats } = this.props;
+    const makeGoats = goats.map(goat => (
+      <h2>{ goat.name }</h2>
+    ));
+    return (
+      <div>
+        { makeGoats }
+      </div>
+    );
+  }
+}
+
+export default GoatCorral;
